688994d73269f5cfbba259473c984d5768afc8be
sadeq <tsts220g@gmail.com>
2020-08-23 07:11:16 +0000

    concatanate fixed

--- concatanate.cpp
+++ concatanate.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 struct node{
 node* next;
 int data;
@@ -8,21 +11,66 @@ Dnode* next;
 Dnode* prev;
 int data;
 };
-node* concatanate(node* f1,node* f2,node* f3){
+class DLinkedList{
+	private:
+		Dnode *first;
+	public:	
+DLinkedList(){
+first=NULL;		
+	     }
+void addNode(int x){
+	Dnode* newNode=new Dnode;
+	newNode->data=x;
+if( first == NULL ){
+	first=newNode;
+	newNode->prev=NULL;
+	newNode->next=NULL;
+}else{
+Dnode* temp=first;
+while(temp->next != NULL )
+	temp=temp->next;
+temp->next=newNode;
+newNode->prev=temp;
+}}
+void display(){
+Dnode* temp=first;
+while(temp != NULL ){
+cout<<(temp->data)<<"   ";
+temp=temp->next;    }
+             }
+void concatanate(Dnode* f1,Dnode* f2,Dnode* f3){
 
-	node* temp=f1;
-while(temp->next =! NULL)
+	Dnode* temp=f1;
+while(temp->next != NULL)
 	temp=temp->next;
 temp->next=f2;
-node* temp2=f2;
-while(temp2->next =! NULL)
+f2->prev=temp;
+Dnode* temp2=f2;
+while(temp2->next != NULL)
 	temp2=temp2->next;
 temp2->next=f3;
-return f1;
+f3->prev=temp2;
 }
-void deleteLast(Dnode* head){
-	Dnode* temp=head;
-while(temp->next->next =! NULL)
+void deleteLast(){
+	Dnode* temp=first;
+while((temp->next->next) != NULL)
 	temp=temp->next;
+temp->next->prev=NULL;
+delete (temp->next);
+display();
 temp->next=NULL;
+}};
+int main(){
+DLinkedList a;
+a.display();
+cout<<endl;
+a.addNode(1);
+a.addNode(2);
+a.addNode(3);
+a.display();
+a.deleteLast();
+cout<<endl;
+a.display();
+
+return 0;
 }
